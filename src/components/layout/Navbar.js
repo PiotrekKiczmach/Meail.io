7eb17a74286a215be075788c7e7ae4c5f90f2096
PiotrekKiczmach <kiczmach.p@gmail.com>
2018-12-05 20:48:18 +0100

    Added redux

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 import M from 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
-import user from '../../images/yuna.jpg';
 import bg from '../../images/office.jpg';
 
 class Navbar extends Component {
@@ -21,16 +21,16 @@ class Navbar extends Component {
                             <div className="background">
                                 <img src={bg} alt=""/>
                             </div>
-                            <a href="#user"><img className="circle" src={user} alt=""/></a>
+                            <NavLink to='/' className="btn btn-floating lighten-1 btn-large" >PK</NavLink>
                             <a href="#name"><span className="white-text name">John Kovalski</span></a>
                             <a href="#email"><span className="white-text email">j.koval@gmail.com</span></a>
                         </div>
                     </li>
-                    <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
+                    <li><a href="#!"><i className="material-icons">settings</i>Ustawienia</a></li>
                     <li><a href="#!">Second Link</a></li>
                     <li><div className="divider"></div></li>
                     <li><p className="subheader">Subheader</p></li>
-                    <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+                    <li><a onClick=""><i className="material-icons">exit_to_app</i>Wyloguj</a></li>
                 </ul>
                 <a href="#!" data-target="slide-out" className="sidenav-trigger" alt=""><i className="material-icons small">menu</i></a>
             </span>
